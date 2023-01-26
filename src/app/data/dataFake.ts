2bfe0ba286a170a8b27981a1026730003e1f6ea3
Thiago Bernardi Parascandalo <thiago520@gmail.com>
2023-01-26 20:33:33 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id":"1",        
+        "title": "NOVO HOMEM DE FERRO ANUNCIADO",
+        "description": "Marvel studios anuncia novo filme do homem de ferro",
+        "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    },
+    {
+        "id":"2",        
+        "title": "Nova Série anunciada",
+        "description": "olá",
+        "photoCover": "https://media.contentapi.ea.com/content/dam/news/www-ea/images/2022/09/ea-motive-new-title-teaser-16x9-featured.jpg.adapt.crop16x9.575p.jpg"
+    },
+    {
+        "id":"3",        
+        "title": "Novo filme do Pantera Negra lançado!",
+        "description": "olá",
+        "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/79C8D57C3AEC4FE8E46F1E22F02523EEC968FFEE3E3E7FD9DBE4E16B6C0A0157/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    },
+    {
+        "id":"4",        
+        "title": "Marvel revela os novos X-Men em trailer",
+        "description": "olá",
+        "photoCover": "https://observatoriodocinema.uol.com.br/wp-content/uploads/2020/03/x-men-1.jpg"
+    },
+]
